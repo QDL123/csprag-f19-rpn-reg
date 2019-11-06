bf55d77bf98bce22a5e813ae86b4a8fd011ebaed
qdl <qdl@umich.edu>
2019-11-06 18:42:48 -0500

    Pow test
    There is now a pow test but no implementation

--- test_rpn.py
+++ test_rpn.py
@@ -9,15 +9,16 @@ class TestBasics(unittest.TestCase):
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
-#   def test_multiply(self):
-#      result = rpn.calculate("5 3 *")
-#      self.assertEqual(15, result)
+    def test_multiply(self):
+        result = rpn.calculate("5 3 *")
+        self.assertEqual(15, result)
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
-    def test_pow(self):
-        result = rpn.calculate("2 3 ^")
-        self.assertEqual(8, result)
+    def test_pow(self):
+        result = rpn.calculate("2 3 ^")
+        self.assertEqual(8, result)
+
 
 if __name__ == "__main__":
 	unittest.main()
